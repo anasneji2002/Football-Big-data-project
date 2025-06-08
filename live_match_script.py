5b444b4494d96acd339a1fa8eafb675685ab4327
anasneji2002 <mohamedanas.neji@medius.com>
2025-06-08 14:40:56 +0100

    add data processor interface + script and sample data for live match endpoint

---
+++ live_match_script.py
@@ -0,0 +1,25 @@
+import requests
+
+api_key_to_use = "xxx"
+headers = {
+    'x-api-key': api_key_to_use  # Replace with your actual key
+}
+
+r = requests.get("https://api.sportradar.com/soccer-extended/trial/v4/stream/statistics/subscribe",
+    params = {'format': 'json', 'sport_event_id': "xxxx"},
+    headers= headers,
+    allow_redirects=False)
+
+if(r.status_code != 302): ## most likely the api key is used up for the day so we try using a new one
+    print(f"Error:{r.json()}")
+
+redirect_url = r.headers['Location']
+r = requests.get(redirect_url, stream=True)
+
+if(r.status_code != 200): ## most likely the api key is used up for the day so we try using a new one
+    print(f"Error:{r.json()}")
+
+for line in r.iter_lines():
+    # filter out keep-alive new lines
+    if line:
+        print(line)
